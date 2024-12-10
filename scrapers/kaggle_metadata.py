26fc1f4071cbf817a60921779cd39f7e039f4594
Emil Daniel Badura <badura@tu-berlin.de>
2024-12-10 11:22:34 +0100

    rework script

--- scrapers/kaggle_metadata.py
+++ scrapers/kaggle_metadata.py
@@ -1,48 +1,53 @@
 import pickle
 import time
-
-import kaggle
-import pandas as pd
-import tqdm
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from kaggle.api.kaggle_api_extended import KaggleApi
 from kaggle.rest import ApiException
+import tqdm
+import os
 
-kaggle.api.authenticate()
+# authentification
+api = KaggleApi()
+api.authenticate()
 
-username_slug = pd.read_csv("../data/username_slug.csv")
+API_LIMIT = 200 # max requests per minute
+MAX_PAGES = 100  # max pages
+MAX_WORKERS = 10
+OUTPUT_DIR = "../data"
+metadata = []    # metadata list
+errors = []      # error list
 
-metadata = []
-errors = []
-for user in tqdm.tqdm(username_slug["UserName"].unique()):
+def fetch_page(page):
     try:
-        page = 1
-        while True:
-            results = kaggle.api.dataset_list(
-                sort_by="hottest",
-                file_type="csv",
-                license_name="all",
-                user=user,
-                page=page,
-            )
-            if len(results) >= 1:
-                metadata.extend(results)
-            if len(results) < 10:
-                break
-
-            page += 1
-            if page > 500:
-                print(
-                    f"User {user} has more than 10000 datasets. The API won't show all of them."
-                )
-
-            time.sleep(0.1)
+        results = api.dataset_list(
+            sort_by="hottest",  
+            file_type="csv",     
+            license_name="all", 
+            page=page,           
+        )
+        time.sleep(60 * MAX_WORKERS / API_LIMIT)
+        return results
     except ApiException as e:
-        if e.reason == "Unauthorized":
-            raise e
-        else:
-            errors.append((user, e))
+        return {"error": e, "page": page}
 
-with open("../data/kaggle_metadata.pkl", "wb") as file:
+with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+    futures = {executor.submit(fetch_page, page): page for page in range(1, MAX_PAGES + 1)}
+    for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
+        result = future.result()
+        if isinstance(result, list):
+            metadata.extend(result)  # save success
+        else:
+            errors.append(result)    # save errors
+
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
+# save results
+with open(OUTPUT_DIR + "/kaggle_metadata.pkl", "wb") as file:
     pickle.dump(metadata, file)
 
-with open("../data/kaggle_errors.pkl", "wb") as file:
+with open(OUTPUT_DIR + "/kaggle_errors.pkl", "wb") as file:
     pickle.dump(errors, file)
+
+print(f"{len(metadata)} metadata collected")
+print(f"{len(errors)} errors occurred")
