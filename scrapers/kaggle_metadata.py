1a520646e72f7f67b1468d7ff64322a6bc28d7fd
Emil <badura@tu-berlin.de>
2024-12-13 02:06:06 +0100

    rework script after meeting

--- scrapers/kaggle_metadata.py
+++ scrapers/kaggle_metadata.py
@@ -1,104 +1,103 @@
-from contextlib import redirect_stderr
 import tqdm
 import os
 import requests
 import json
 import re
 import time
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from kaggle.api.kaggle_api_extended import KaggleApi, ApiException
-from io import StringIO
-#import mlcroissant as mlc # causes NoneType error in the end
- 
+from kaggle.api.kaggle_api_extended import KaggleApi
+import pandas as pd
+
 # authentification
 api = KaggleApi()
 api.authenticate()
 
-API_LIMIT = 200 # max requests per minute
-MAX_PAGES = 2  # max pages
-MAX_WORKERS = 1
-MAX_SIZE = 100000 # max datasets size in bytes
-OUTPUT_DIR = "../kaggle_datasets"
+API_LIMIT = 600 # max requests per minute
+OUTPUT_DIR = "../kaggle_metadata"   
+METAKAGGLE_DIR = "../data" 
 
-# transform URIRef("https://...") keys
-def edit_json_data(json: dict):
-    new_json = {}
-    for key, value in json.items():
-        string_key = str(key)
-        last_slash_index = string_key.rfind('/')
-        if last_slash_index == -1:
-            new_json[key] = value
-        else: 
-            new_json[string_key[last_slash_index + 1:]] = value
-    return new_json
+def download_meta_kaggle_dataset():
+    REF = "kaggle/meta-kaggle"
+    os.makedirs(METAKAGGLE_DIR, exist_ok=True)
+    if not os.path.exists(os.path.join(METAKAGGLE_DIR, "Datasets.csv")):
+        print("Downloading Datasets.csv...")
+        api.dataset_download_file(REF, file_name="Datasets.csv", path=METAKAGGLE_DIR)
+    if not os.path.exists(os.path.join(METAKAGGLE_DIR, "DatasetVersions.csv")):
+        print("Downloading DatasetVersions.csv...")
+        api.dataset_download_file(REF, file_name="DatasetVersions.csv", path=METAKAGGLE_DIR)
+    if not os.path.exists(os.path.join(METAKAGGLE_DIR, "Users.csv")):
+        print("Downloading Users.csv...")
+        api.dataset_download_file(REF, file_name="Users.csv", path=METAKAGGLE_DIR)
+
+def create_username_slug():
+    if os.path.exists(os.path.join(METAKAGGLE_DIR, "username_slug.txt")):
+        return
+    print("Creating username_slug.txt...")
+    username_slug = pd.read_csv(os.path.join(METAKAGGLE_DIR, "Datasets.csv"), dtype={"CurrentDatasetVersionId": "Int64", "OwnerUserId": "Int64"})
+    username_slug = username_slug[["Id", "CreatorUserId", "OwnerUserId", "CurrentDatasetVersionId"]]
+    username_slug = username_slug.rename(columns={"Id": "DatasetId"})
     
+    dataset_versions = pd.read_csv(os.path.join(METAKAGGLE_DIR, "DatasetVersions.csv"))
+    dataset_versions = dataset_versions[["Id", "CreatorUserId", "VersionNumber", "Slug"]]
+    dataset_versions = dataset_versions.rename(columns={"Id": "DatasetVersionId"})
+    
+    users = pd.read_csv(os.path.join(METAKAGGLE_DIR, "Users.csv"))
+    users = users[["Id", "UserName"]]
+    users = users.rename(columns={"Id": "UserId"})
+    
+    merged = username_slug.merge(dataset_versions, how="left", left_on=["CurrentDatasetVersionId"], right_on=["DatasetVersionId"],).merge(users, how="left", left_on="OwnerUserId", right_on="UserId")
+    merged.dropna(subset=["UserName", "Slug"], inplace=True)
+    merged = merged[["UserName", "Slug"]]
+    with open(os.path.join(METAKAGGLE_DIR, "username_slug.txt"), "w") as file:
+        for _, row in merged.iterrows():
+            file.write(f"{row['UserName']}/{row['Slug']}\n") 
+    print("finished!")
 
 def get_croissant_metadata(ref):
     url = "https://www.kaggle.com/datasets/" + ref + "/croissant/download"
     response_string = requests.get(url).content.decode("utf-8") 
     return json.loads(response_string)
-    #fnull = StringIO()
-    #with redirect_stderr(fnull): # hide annoying err prints
-    #    response = mlc.Dataset(url).metadata.jsonld
-    #return edit_json_data(response)
-
 
 def sanitize_filename(filename):
     return re.sub(r'[<>:"/\\|?*]', '_', filename)
 
-
-def fetch_page(page):
-    try:
-        results = api.dataset_list(
-            sort_by="hottest",    
-            file_type="csv",     
-            license_name="all", 
-            page=page,
-            max_size=MAX_SIZE
-        )
-        metadata = [{"ref": result.ref, "jsonld": get_croissant_metadata(result.ref)} for result in results]
-        time.sleep(60 * MAX_WORKERS / API_LIMIT)
-        return metadata
-    except ApiException as e:
-        return {"error": e, "page": page}
-    
 def save_metadata(metadata):
     os.makedirs(OUTPUT_DIR, exist_ok=True)
-    for m in metadata:
-        dirname = f"{sanitize_filename(m['jsonld']['name'])}"
-        dirpath = os.path.join(OUTPUT_DIR, dirname)
-        os.makedirs(dirpath, exist_ok=True)
-        with open(os.path.join(dirpath, dirname + ".json"), "w") as json_file:
-            json.dump(m, json_file, indent=4)
+    dirname = f"{sanitize_filename(metadata['jsonld']['name'])}"
+    dirpath = os.path.join(OUTPUT_DIR, dirname)
+    os.makedirs(dirpath, exist_ok=True)
+    with open(os.path.join(dirpath, dirname + ".json"), "w") as json_file:
+        json.dump(metadata, json_file, indent=4)
 
 def collect_metadata():
-    metadata_counter = 0
-    errors = []
-    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        futures = {executor.submit(fetch_page, page): page for page in range(1, MAX_PAGES + 1)}
-        for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
-            result = future.result()
-            if isinstance(result, list):
-                metadata_counter += len(result) # save success
-                save_metadata(result)
-            else:
-                errors.append(result)  # save errors
-    return metadata_counter, errors
-
-def print_errors(errors):
-    print(f"{len(errors)} errors occurred")
-    if len(errors) == 0:
-        return
-    print("Error summmary:")
-    for e in errors:
-        print(f"Page {e['page']}: {e['error']}")
+    metadata = 0
+    errors = 0
+    
+    with open(os.path.join(METAKAGGLE_DIR, "username_slug.txt"), "r") as file:
+        total_lines = sum(1 for _ in file)
+    
+    with open(os.path.join(METAKAGGLE_DIR, "username_slug.txt"), "r") as file, tqdm.tqdm(total=total_lines, desc="Processing datasets") as progress:
+        for line in file:
+            ref = line.strip()
+            try:
+                croissant_sample = get_croissant_metadata(ref)
+                save_metadata({"ref": ref, "jsonld": croissant_sample})
+                metadata += 1
+                time.sleep(int(60 / API_LIMIT))
+            except Exception as e:
+                with open(os.path.join(METAKAGGLE_DIR, "error_datasets.txt"), "a") as file:
+                    file.write(f"{ref},{e}\n") 
+                errors += 1
+            progress.update(1)
+            if progress.n > 100:
+                break
+    return metadata, errors
 
 def main():
-    print(f"collecting metadata for {MAX_PAGES} pages using {MAX_WORKERS} workers...")
-    metadata_counter, errors = collect_metadata()
-    print(f"{metadata_counter} metadata collected.")
-    print_errors(errors)
-
-
+    download_meta_kaggle_dataset()
+    create_username_slug()
+    metadata, errors = collect_metadata()
+    print(f"{metadata} metadata collected.")
+    print(f"{errors} errors occurred")
+    
 if __name__ == "__main__":
     main()
