90a8ad4ea02ae8bb844420c1762909814b85961b
Tenebrae311 <badura@campus.tu-berlin.de>
2025-02-24 14:29:09 +0100

    first version of multiprocessing + documentation

--- dataset_scrapers/kaggle/create_histograms.py
+++ dataset_scrapers/kaggle/create_histograms.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import sys
+import multiprocessing as mp
 from collections import Counter
 from pathlib import Path
 from typing import Any
@@ -8,8 +9,8 @@ from typing import Any
 import cchardet
 import numpy as np
 import pandas as pd
-import tqdm
-
+from tqdm import tqdm
+from time import perf_counter
 
 class HistogramCreator:
     def __init__(
@@ -20,6 +21,7 @@ class HistogramCreator:
         self.max_count = max_count
         self.bin_count = bin_count
         self.error_count = 0
+        self.num_processes = mp.cpu_count()
 
     def analyze_csv_file(self, path: Path) -> tuple[str, str]:
         """Analyze a CSV file and return its encoding and separator."""
@@ -62,78 +64,84 @@ class HistogramCreator:
         return round(score / max_score, 2)
 
     def process_dataset(self, path: Path) -> None:  # noqa: C901
-        # get metadata
-        metadata_path = path / "croissant_metadata.json"
-        with metadata_path.open(encoding="utf-8") as file:
-            metadata: dict[str, Any] = json.load(file)
-        records: list[dict[str, Any]] = metadata["recordSet"]
+        try: 
+            # get metadata
+            metadata_path = path / "croissant_metadata.json"
+            with metadata_path.open(encoding="utf-8") as file:
+                metadata: dict[str, Any] = json.load(file)
+            records: list[dict[str, Any]] = metadata["recordSet"]
 
-        score = self.calculate_usability(metadata)
-        metadata["usability"] = score
+            score = self.calculate_usability(metadata)
+            metadata["usability"] = score
 
-        for file_record in records:
-            csv_file = path / file_record["@id"].replace("+", " ").replace("/", "_")
-            encoding, separator = self.analyze_csv_file(csv_file)
-            df = pd.read_csv(
-                csv_file, encoding=encoding, sep=separator, engine="python", on_bad_lines="skip"
-            )
+            for file_record in records:
+                csv_file = path / file_record["@id"].replace("+", " ").replace("/", "_")
+                encoding, separator = self.analyze_csv_file(csv_file)
+                df = pd.read_csv(
+                    csv_file, encoding=encoding, sep=separator, engine="python", on_bad_lines="skip"
+                )
 
-            # remove unnecessary spaces
-            df.columns = df.columns.str.strip()
-            for i, column in enumerate(file_record["field"]):
-                data_type = column["dataType"][0].rsplit(":", 1)[-1].lower()
-                column_name = column["name"]
-                # catch case where column has empty name
-                if column_name == "":
-                    column_name = f"Unnamed: {i}"
-                data = df[column_name].dropna().to_list()
-                # case for numeric columns
-                if data_type in ["int", "integer", "float"]:
-                    if isinstance(data[0], str):
-                        try:
-                            # catch case where 1923423 = "1,923,423"
-                            # NOTE: This causes issues with German-style decimal separators
-                            data = [float(d.replace(",", "")) for d in data]
-                        except Exception:
-                            # map strings to numbers
-                            unique_strings = list(set(data))
-                            mapping = {string: idx for idx, string in enumerate(unique_strings)}
-                            data = [mapping[item] for item in data]
-                    # create histogram
-                    densities, bins = np.histogram(data, density=True, bins=self.bin_count)
-                    # save hist
-                    column["histogram"] = {
-                        "bins": list(bins),
-                        "densities": list(densities / np.sum(densities)),
-                    }
-                    column["statistics"] = df[column_name].dropna().describe().to_dict()
-                # case for text columns
-                elif data_type == "text":
-                    n_unique = len(set(data))
-                    top_10 = dict(Counter(data).most_common(10))
-                    column["n_unique"] = n_unique
-                    column["most_common"] = top_10
-                elif data_type == "boolean":
-                    if isinstance(data[0], str):
-                        data = [d.lower() for d in data]
-                        if data[0] == "true" or data[0] == "false":
-                            data = [d == "true" for d in data]
-                        elif data[0] == "yes" or data[0] == "no":
-                            data = [d == "yes" for d in data]
-                        elif data[0] == "t" or data[0] == "f":
-                            data = [d == "t" for d in data]
-                    elif isinstance(data[0], int | float):
-                        max_value = max(data)
-                        data = [d == max_value for d in data]
-                    n_true = sum(data)
-                    n_false = len(data) - n_true
-                    column["n_true"] = n_true
-                    column["n_false"] = n_false
+                # remove unnecessary spaces
+                df.columns = df.columns.str.strip()
+                for i, column in enumerate(file_record["field"]):
+                    data_type = column["dataType"][0].rsplit(":", 1)[-1].lower()
+                    column_name = column["name"]
+                    # catch case where column has empty name
+                    if column_name == "":
+                        column_name = f"Unnamed: {i}"
+                    data = df[column_name].dropna().to_list()
+                    # case for numeric columns
+                    if data_type in ["int", "integer", "float"]:
+                        if isinstance(data[0], str):
+                            try:
+                                # catch case where 1923423 = "1,923,423"
+                                # NOTE: This causes issues with German-style decimal separators
+                                data = [float(d.replace(",", "")) for d in data]
+                            except Exception:
+                                # map strings to numbers
+                                unique_strings = list(set(data))
+                                mapping = {string: idx for idx, string in enumerate(unique_strings)}
+                                data = [mapping[item] for item in data]
+                        # create histogram
+                        densities, bins = np.histogram(data, density=True, bins=self.bin_count)
+                        # save hist
+                        column["histogram"] = {
+                            "bins": list(bins),
+                            "densities": list(densities / np.sum(densities)),
+                        }
+                        column["statistics"] = df[column_name].dropna().describe().to_dict()
+                    # case for text columns
+                    elif data_type == "text":
+                        n_unique = len(set(data))
+                        top_10 = dict(Counter(data).most_common(10))
+                        column["n_unique"] = n_unique
+                        column["most_common"] = top_10
+                    elif data_type == "boolean":
+                        if isinstance(data[0], str):
+                            data = [d.lower() for d in data]
+                            if data[0] == "true" or data[0] == "false":
+                                data = [d == "true" for d in data]
+                            elif data[0] == "yes" or data[0] == "no":
+                                data = [d == "yes" for d in data]
+                            elif data[0] == "t" or data[0] == "f":
+                                data = [d == "t" for d in data]
+                        elif isinstance(data[0], int | float):
+                            max_value = max(data)
+                            data = [d == max_value for d in data]
+                        n_true = sum(data)
+                        n_false = len(data) - n_true
+                        column["n_true"] = n_true
+                        column["n_false"] = n_false
+                    elif data_type == "date":
+                        pass
 
-        # write metadata to target_dir
-        file_name = "/".join(str(path).split("/")[-2:]).replace("/", "_") + ".json"
-        with (self.target_dir / file_name).open("w") as file:
-            json.dump(metadata, file, indent=4, ensure_ascii=False)
+            # write metadata to target_dir
+            file_name = "/".join(str(path).split("/")[-2:]).replace("/", "_") + ".json"
+            with (self.target_dir / file_name).open("w") as file:
+                json.dump(metadata, file, indent=4, ensure_ascii=False)
+        except Exception as e:
+            print(f"Error occurred with {path}: {e}")
+            self.error_count += 1   
 
     def start(self) -> None:
         process_list: list[Path] = []
@@ -141,17 +149,13 @@ class HistogramCreator:
             if len(list(path.parent.iterdir())) > 1:
                 process_list.append(path.parent)
         process_list = process_list[: min(self.max_count, len(process_list))]
-        with tqdm.tqdm(total=len(process_list), desc="processing datasets") as progress:
-            for dataset_path in process_list:
-                try:
-                    self.process_dataset(dataset_path)
-                except Exception as e:
-                    print(f"Error occurred with {dataset_path}: {e}")
-                    self.error_count += 1
-                progress.update(1)
+        process_len = len(process_list)
+        with mp.Pool(processes=self.num_processes) as pool:
+            list(tqdm(pool.imap_unordered(self.process_dataset, process_list), total=process_len))
+
         print(
-            f"{len(process_list) - self.error_count} of {len(process_list)} datasets processed"
-            f"({round((len(process_list) - self.error_count) / len(process_list) * 100)}%)"
+            f"{process_len - self.error_count} of {process_len} datasets processed "
+            f"({round((process_len - self.error_count) / process_len * 100)}%)"
         )
 
 
---
+++ docs/kaggle.md
@@ -0,0 +1,47 @@
+
+## Usage for Kaggle
+
+### 0. Get access to the kaggle api
+Follow the steps provided [`here`](https://www.kaggle.com/docs/api) under "Authentification": 
+
+### 1. Download the metadata
+
+Corresponding script: `kaggle/download_metadata.py`
+
+Available arguments: 
+- keyword `-k`or `--keyword` (string): Metadata will be downloaded from kaggle matching this keyword. Defaults to all metadata available from the metakaggle dataset.
+- start index `-i` or `--start-index` (integer): Used to continue the download from a certain point. Defaults to 0.
+- workers `-w` or `--workers`(integer): If set to >1, the script will use multithreading to download multiple metadata at the same time. Defaults to 1. Warning: Kaggle api has limited requests per minute.
+- max pages `--max-pages` (integer): Maximum number of pages to look for metadata if keyword is provided. Defaults to 100.
+- data directory `--data-dir` (string): Desired path where the data directory should be created, which is mainly used to save the metakaggle dataset and refs to datasets where errors occurred during download. Defaults to `../data`.
+- metadata directory `--output` (string): Desired path to the directory where the metadata will be collected. Defaults to `../kaggle_metadata`
+
+### 2. Analyze the metadata (optional)
+
+Corresponding script: `kaggle/analyze_metadata.py`
+
+Available arguments:
+- source dir `--source` (string): Path to the metadata to be analyzed. Defaults to `../kaggle_metadata`.
+- output dir `--output` (string): Desired path to the directory where the plots will be saved. Defaults to `../plots`.
+- max files `--max-files` (integer): Upper limit for the x-Axis in the plot showing the distribution of CSV files in a dataset. Defaults to 100.
+- max size `--max-size` (integer): Upper limit for the x-Axis in the plot showing the distribution of file sizes of .zip dataset files.
+- max columns `--max-columns` (integer): Upper limit for the x-Axis in the plot showing the distribution of columns in csv files. 
+- show plots `--show-plots` (bool): Whether all plots should be rendered after analysis. Defaults to `false`.
+
+### 3. Download datasets
+
+Corresponding script: `kaggle/download_datasets.py`
+
+Available arguments:
+- path `--path` (string): Path to metadata where the datasets will be downloaded into. Defaults to `../kaggle_metadata`.
+- start index `-i` or `--start-index` (integer): Used to continue the download from a certain point. Defaults to 0.
+
+### 4. Create histograms
+
+Corresponding script: `kaggle/create_histograms.py`
+
+Available arguments:
+- source dir `--source` (string): Path to metadata with datasets. Defaults to `../kaggle_metadata`.
+- result dir `--result` (string): Desired path to the directory where the metadata enriched with histograms will be collected. Defaults to `../croissant`.
+- max datasets `--max-datasets` (integer): Maximum number of datasets to be processed. Defaults to all datasets available.
+- bin count `--bin-count` (integer): Number of bins used for every histogram. Defaults to 10.
